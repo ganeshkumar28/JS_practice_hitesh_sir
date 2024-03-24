c6765f90db864147b5648d08ede9329e0baa566a
ganeshkumar28 <uganeshkumar08@gmail.com>
2024-03-24 18:22:02 +0530

    Updated result part

--- 02_BMI_Calculator/script.js
+++ 02_BMI_Calculator/script.js
@@ -17,6 +17,11 @@ form.addEventListener('submit',(e)=>{
     }
     else{
         const bmi = (weight/((height*height)/10000)).toFixed(2);
-        result.innerHTML = `<span>${bmi}</span>`;
+        if(bmi <=18.6)
+        result.innerHTML = `<span>${bmi} , You come under <span style="color: orange;">Under Weight</span> category </span> `;
+        else if (bmi >18.6 && bmi <= 24.9)
+        result.innerHTML = `<span>${bmi} , You come under <span style="color: green;">Normal Weight</span> category </span> `;
+        else
+        result.innerHTML = `<span>${bmi} , You come under <span style="color: Red;">Over Weight</span> category </span> `;
     }
 })
